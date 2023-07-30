025b7a4b226eddbc54d90a348d94029f417909fd
Samuel Akins <akinyemisamuel170@gmail.com>
2023-07-30 22:31:34 +0100

    fixed countdowns

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("March 25, 2021 15:37:25").getTime();
+        var finalDate =  new Date("August 05, 2023 00:00:00").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)

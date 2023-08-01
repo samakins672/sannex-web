87311102944c4cf061ea841eb650e146949c873a
Samuel Akins <akinyemisamuel170@gmail.com>
2023-08-01 21:13:37 +0100

    added error reporting

--- mail.php
+++ mail.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;

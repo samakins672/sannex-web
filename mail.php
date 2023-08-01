2ff6d6c1b2e00e24f42965a3689f0b32c0516ddf
Samuel Akins <akinyemisamuel170@gmail.com>
2023-08-01 20:58:27 +0100

    checking ajax error 4

--- mail.php
+++ mail.php
@@ -15,23 +15,23 @@ $from = $_POST['email'];
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'twentythree.qservers.net';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'info@sannex.ng';                     //SMTP username
-    $mail->Password   = 'Sannex.info1';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+    $mail->isSMTP(); //Send using SMTP
+    $mail->Host = 'twentythree.qservers.net'; //Set the SMTP server to send through
+    $mail->SMTPAuth = true; //Enable SMTP authentication
+    $mail->Username = 'info@sannex.ng'; //SMTP username
+    $mail->Password = 'Sannex.info1'; //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+    $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom("$from", 'Sucbscriber_'.time());
-    $mail->addAddress('info@sannex.ng', 'Sannex Inc');     //Add a recipient
+    $mail->setFrom("$from", 'Sucbscriber_');
+    $mail->addAddress('info@sannex.ng', 'Sannex Inc'); //Add a recipient
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true); //Set email format to HTML
     $mail->Subject = 'Launch Notification';
-    $mail->Body    = "Please notify " . $from;
+    $mail->Body = "Please notify " . $from;
     // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();

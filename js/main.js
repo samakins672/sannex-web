6bc9539e8d10037a0f98295ef96866c4f71ec215
Samuel Akins <akinyemisamuel170@gmail.com>
2023-08-01 20:06:48 +0100

    extend launch date to August 10

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("August 05, 2023 00:00:00").getTime();
+        var finalDate =  new Date("August 10, 2023 00:00:00").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)

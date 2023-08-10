be0d5eb9b7ff9e17948fd03ebed414924e46e567
Samuel Akins <akinyemisamuel170@gmail.com>
2023-08-10 17:00:20 +0100

    extends launch date

--- mail.php
+++ mail.php
@@ -10,9 +10,6 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// //Load Composer's autoloader
-// require 'vendor/autoload.php';
-
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
